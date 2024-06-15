58ba013d650f19ea2ff336f56475d537146dd5ea
Olga Rozmetova <olga.rozmetova@gmail.com>
2024-06-15 19:45:15 +0200

    first commit

---
+++ dist/index.09c24910.js
@@ -0,0 +1,42 @@
+"use strict";
+function userScroll() {
+    const navbar = document.querySelector(".navbar");
+    const toTopBtn = document.querySelector("#to-top");
+    window.addEventListener("scroll", ()=>{
+        if (window.scrollY > 50) {
+            navbar.classList.add("navbar-sticky");
+            toTopBtn.classList.add("show");
+        } else {
+            navbar.classList.remove("navbar-sticky");
+            toTopBtn.classList.remove("show");
+        }
+    });
+}
+// Scroll to Top
+function scrollToTop() {
+    document.body.scrollTop = 0; // for Safari
+    document.documentElement.scrollTop = 0; // for others
+}
+// Increment Statistics
+function incrementStats() {
+    const counters = document.querySelectorAll(".counter");
+    counters.forEach((counter)=>{
+        counter.innerText = 0;
+        const updateCounter = ()=>{
+            const target = +counter.getAttribute("data-target");
+            const c = +counter.innerText;
+            const increment = target / 200;
+            if (c < target) {
+                counter.innerText = Math.ceil(c + increment);
+                setTimeout(updateCounter, 1);
+            } else counter.innerText = target;
+        };
+        updateCounter();
+    });
+}
+// Event Listeners
+document.addEventListener("DOMContentLoaded", userScroll);
+document.addEventListener("DOMContentLoaded", incrementStats);
+document.querySelector("#to-top").addEventListener("click", scrollToTop);
+
+//# sourceMappingURL=index.09c24910.js.map
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Yavin Website
+
+Bootstrap website for a company that creates and customizes office spaces. This website is built with Bootstrap and SCSS. It uses [Font Awesome](https://fontawesome.com/) for icons.
+
+## Features
+
+- Clean design with background images
+- Responsive design
+- Custom JavaScript for the stat counter
+- Nav scroll effect
+- Sticky navigation
+- Testimonial carousel
+- Contact form
+- Inner article page
+- Font awesome icons
+
+<img src="./images/screen.png" />
